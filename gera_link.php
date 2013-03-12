02145bf3def7a76100259267b43a5890220e7b58
Marciso <marciso.gonzalez@gmail.com>
2013-03-12 13:23:54 -0400

    corrigido campos input e label no arquivo gera_link

--- gera_link.php
+++ gera_link.php
@@ -23,15 +23,19 @@
 				$nome=readdir($dir);
 			}
 			sort($arquivo);
+			echo "<strong>Arquivos convertido anteriormente</strong><br />";
 			echo "<select name='arquivo'>";
 			foreach($arquivo as $arq){
 				//echo '<a href='.$diretorio.'/'.$arq.'>'.$arq.'</a><br>';
 				echo "<option value='$arq'>$arq</option>";
 			}
+			echo "</select><br />";
 			?>
 			<input type="hidden" name="MAX_FILE_SIZE" value="1000000" class="btn btn-primary"/>
-			Selecione o arquivo: <input name="arquivo" type="file" size="50"/>
-			<button type="submit" class="btn btn-primary">Converter Links</button>
+			<label for="arquivo">Selecione o arquivo: </label><br />
+			<input name="arquivo" type="file" size="50"/>
+			<br />
+			<button type="submit" class="btn btn-primary">Converter</button>
 		</form>
 	</body>
 </html>
